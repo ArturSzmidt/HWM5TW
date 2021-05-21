cd79b2243716575f1f6116c650d21a1ea2163d77
Tommy-Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-05-21 15:59:10 +0100

    backofficeFix

--- hw-tw-d5/src/components/BackOffice.jsx
+++ hw-tw-d5/src/components/BackOffice.jsx
@@ -2,58 +2,87 @@ import React, { Component } from "react"
 
 export default class backOffice extends Component {
 	state = {
+		isLoading: false,
 		name: "",
 		brand: "",
-		imageUrl: "",
 		price: "",
 		description: "",
+		category: "",
+		file: "",
+		imgUrl: "",
 	}
 
 	getName = (e) => {
 		this.setState({ name: e.target.value })
-		console.log(this.state)
 	}
 	getBrand = (e) => {
 		this.setState({ brand: e.target.value })
-		console.log(this.state)
 	}
-	getImgUrl = (e) => {
-		this.setState({ imageUrl: e.target.value })
-		console.log(this.state)
-	}
-	getPrice = (e) => {
+	getPrice = (e) => {``
 		this.setState({ price: e.target.value })
-		console.log(this.state)
 	}
 	getDescription = (e) => {
 		this.setState({ description: e.target.value })
-		console.log(this.state)
+	}
+	getCategory = (e) => {
+		this.setState({ category: e.target.value })
+	}
+	getFile = (e) => {
+		this.setState({ file: e.target.files[0] })
 	}
 
 	handleSubmit = async () => {
 		const postProduct = {
 			name: this.state.name,
 			brand: this.state.brand,
-			imageUrl: this.state.imageUrl,
 			price: this.state.price,
-			description: this.state.description
+			description: this.state.description,
+			category: this.state.category,
 		}
 		try {
-			const response = await fetch(
-				"https://striveschool-api.herokuapp.com/api/product/",
-				{
-					method: "POST",
-					body: JSON.stringify(postProduct),
-					headers: {
-						Authorization:
-							"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMDlkOWIxZjBmYjAwMTVkOTE2ZmYiLCJpYXQiOjE2MjE1OTMxMjMsImV4cCI6MTYyMjgwMjcyM30.KH0U-5IdgCx5VNSAT2DjhGbThKvpTr_YrirAEcJEK-s",
-					},
+			console.log(postProduct)
+			const response = await fetch("http://localhost:3001/products/", {
+				method: "POST",
+				body: JSON.stringify(postProduct),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+
+			if (response.ok) {	
+				//  here it checks if there is an image , if image = true then it posts it
+				console.log("state.file", this.state.file)
+				if (this.state.file) {
+					let data = await response.json()
+					let id = data["_id"]
+					console.log("here id", id)
+					var formdata = new FormData()
+					formdata.append("productImg", this.state.file)
+					try {
+						let res = await fetch(
+							"http://localhost:3001/product/" + id + "/upload",
+							{
+								method: "POST",
+								body: formdata,
+							}
+						)
+						if (res.ok) {
+							let file = await res.json()
+							this.setState({ imgUrl: file.imageUrl })
+							alert("Your Post has been saved!!")
+						} else {
+							alert("There is an error ", res.status)
+						}
+					} catch (error) {
+						alert("error")
+					}
 				}
-			)
-			const post = await response
-			console.log("success!" + post)
+			} else {
+				this.setState({ isLoading: false })
+				alert("Something happened :/", response.status)
+			}
 		} catch (error) {
-			console.log("You have an error posting:", error)
+			alert("FINAL CATCH",error)
 		}
 	}
 
@@ -89,12 +118,12 @@ export default class backOffice extends Component {
 									/>
 								</div>
 								<div className="form-group">
-									<label htmlFor="brand">Img Url</label>
+									<label htmlFor="category">Category</label>
 									<input
-										type="url"
+										type="text"
 										className="form-control"
-										id="imgUrl"
-										onChange={this.getImgUrl}
+										id="category"
+										onChange={this.getCategory}
 										required
 									/>
 								</div>
@@ -117,7 +146,7 @@ export default class backOffice extends Component {
 										type="file"
 										className="form-control"
 										id="file"
-										// onChange={this.getFile}
+										onChange={this.getFile}
 									/>
 								</div>
 								<div className="form-group">
