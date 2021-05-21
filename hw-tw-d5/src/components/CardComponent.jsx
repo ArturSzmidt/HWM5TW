e92636f7d771ddd686a5d712b642afd604630b22
ArturSzmidt <80460700+ArturSzmidt@users.noreply.github.com>
2021-05-21 14:23:10 +0200

    +

--- hw-tw-d5/src/components/CardComponent.jsx
+++ hw-tw-d5/src/components/CardComponent.jsx
@@ -1,33 +1,14 @@
 import React, { Component } from 'react';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
-
-
-
-
-
+// import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import fetchProducts from '../functions/fetchProducts';
+fetchProducts();
 class CardComponent extends Component {
   render() {
     return (
       <div>
-        <Card style={{}}>
-          <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroupItem>Cras justo odio</ListGroupItem>
-            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-          </ListGroup>
-          <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
+        {fetchProducts().map((item) => {
+          return <p>{item.name}</p>;
+        })}
       </div>
     );
   }
--- hw-tw-d5/src/functions/fetchProducts.jsx
+++ hw-tw-d5/src/functions/fetchProducts.jsx
@@ -1,15 +1,16 @@
 export default async function fetchProducts() {
-	let allProducts = []
+  let allProducts = [];
 
-	const response = await fetch(
-		"https://striveschool-api.herokuapp.com/api/product/",
-		{
-			headers: {
-				Authorization:
-					"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMDlkOWIxZjBmYjAwMTVkOTE2ZmYiLCJpYXQiOjE2MjE1OTMxMjMsImV4cCI6MTYyMjgwMjcyM30.KH0U-5IdgCx5VNSAT2DjhGbThKvpTr_YrirAEcJEK-s",
-			},
-		}
-	)
-	const products = await response.json()
-    return products
+  const response = await fetch(
+    'https://striveschool-api.herokuapp.com/api/product/',
+    {
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMDlkOWIxZjBmYjAwMTVkOTE2ZmYiLCJpYXQiOjE2MjE1OTMxMjMsImV4cCI6MTYyMjgwMjcyM30.KH0U-5IdgCx5VNSAT2DjhGbThKvpTr_YrirAEcJEK-s',
+      },
+    }
+  );
+  const products = await response.json();
+  console.log('sdsdadasdasd', products);
+  return products;
 }
