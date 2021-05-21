adc9f24260ac7f11a3d4a227ee6cdd45053e6da6
Tommy-Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-05-21 11:15:10 +0100

    routes

--- hw-tw-d5/src/App.js
+++ hw-tw-d5/src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import BackOffice from './components/BackOffice'
+import Home from './components/Home'
+// import NavBar from './components/navbar'
+
 
 function App() {
-  return <div className="App"></div>;
+	return (
+		<Router basename="/">
+			{/* <NavBar /> */}
+			<Route path="/" exact component={Home} />
+			<Route path="/backoffice" exact component={BackOffice} />
+		</Router>
+	)
 }
 
-export default App;
+export default App
