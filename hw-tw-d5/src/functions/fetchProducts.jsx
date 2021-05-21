d62930c871f826ee29acf7e6c5007bfdd5514420
Hasib <hashhash506@gmail.com>
2021-05-21 16:00:39 +0100

    finalversion

--- hw-tw-d5/src/functions/fetchProducts.jsx
+++ hw-tw-d5/src/functions/fetchProducts.jsx
@@ -1,15 +1,16 @@
 export default async function fetchProducts() {
   let allProducts = [];
 
-  const response = await fetch(
-    'https://striveschool-api.herokuapp.com/api/product/',
-    {
-      headers: {
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMDlkOWIxZjBmYjAwMTVkOTE2ZmYiLCJpYXQiOjE2MjE1OTMxMjMsImV4cCI6MTYyMjgwMjcyM30.KH0U-5IdgCx5VNSAT2DjhGbThKvpTr_YrirAEcJEK-s',
-      },
-    }
-  );
+  // const response = await fetch(
+  //   'https://striveschool-api.herokuapp.com/api/product/',
+  //   {
+  //     headers: {
+  //       Authorization:
+  //         'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMDlkOWIxZjBmYjAwMTVkOTE2ZmYiLCJpYXQiOjE2MjE1OTMxMjMsImV4cCI6MTYyMjgwMjcyM30.KH0U-5IdgCx5VNSAT2DjhGbThKvpTr_YrirAEcJEK-s',
+  //     },
+  //   }
+  // );
+  const response = await fetch('http://localhost:3001/products');
   const products = await response.json();
   products.forEach((product) => {
     allProducts.push(product);
