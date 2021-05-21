f1c0af1b4f96a4af47a0f0ca10f08d8cfb8f6013
Tommy-Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-05-21 13:01:40 +0100

    Backoffice push

--- hw-tw-d5/src/components/BackOffice.jsx
+++ hw-tw-d5/src/components/BackOffice.jsx
@@ -1,17 +1,62 @@
 import React, { Component } from "react"
 
 export default class backOffice extends Component {
+	state = {
+		name: "",
+		brand: "",
+		imgUrl: "",
+		price: "",
+		description: "",
+	}
 
+	getName = (e) => {
+		this.setState({ name: e.target.value })
+		console.log(this.state)
+	}
+	getBrand = (e) => {
+		this.setState({ brand: e.target.value })
+		console.log(this.state)
+	}
+	getImgUrl = (e) => {
+		this.setState({ imgUrl: e.target.value })
+		console.log(this.state)
+	}
+	getPrice = (e) => {
+		this.setState({ price: e.target.value })
+		console.log(this.state)
+	}
+	getDescription = (e) => {
+		this.setState({ description: e.target.value })
+		console.log(this.state)
+	}
 
-	
+	handleSubmit = async () => {
+		const postProduct = {
+			name: this.state.name,
+			brand: this.state.brand,
+			imgUrl: this.state.imgUrl,
+			price: this.state.price,
+			description: this.state.description,
+		}
+		try {
+			const response = await fetch(
+				"https://striveschool-api.herokuapp.com/api/product/",
+				{
+					method: "PUT",
+					body: JSON.parse(postProduct),
+					headers: {
+						Authorization:
+							"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMDlkOWIxZjBmYjAwMTVkOTE2ZmYiLCJpYXQiOjE2MjE1OTMxMjMsImV4cCI6MTYyMjgwMjcyM30.KH0U-5IdgCx5VNSAT2DjhGbThKvpTr_YrirAEcJEK-s",
+					},
+				}
+			)
+			const post = await response.json()
+			console.log("success!" + post)
+		} catch (error) {
+			console.log("You have an error posting:", error)
+		}
+	}
 
-
-
-
-
-
-
-	
 	render() {
 		return (
 			<div>
@@ -29,6 +74,7 @@ export default class backOffice extends Component {
 										type="text"
 										className="form-control"
 										id="productName"
+										onChange={this.getName}
 										required
 									/>
 								</div>
@@ -38,6 +84,7 @@ export default class backOffice extends Component {
 										type="text"
 										className="form-control"
 										id="brand"
+										onChange={this.getBrand}
 										required
 									/>
 								</div>
@@ -47,6 +94,7 @@ export default class backOffice extends Component {
 										type="url"
 										className="form-control"
 										id="imgUrl"
+										onChange={this.getImgUrl}
 										required
 									/>
 								</div>
@@ -56,6 +104,7 @@ export default class backOffice extends Component {
 										type="number"
 										className="form-control"
 										id="price"
+										onChange={this.getPrice}
 										required
 									/>
 									<small className="form-text ">
@@ -70,10 +119,15 @@ export default class backOffice extends Component {
 										rows={3}
 										required
 										defaultValue={""}
+										onChange={this.getDescription}
 									/>
 								</div>
 								<span>
-									<button type="submit" className="btn btn-primary">
+									<button
+										type="button"
+										className="btn btn-primary"
+										onClick={this.handleSubmit}
+									>
 										Submit
 									</button>
 								</span>
